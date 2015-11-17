fd85e01d2436e416c465b477a3f38decb5e2027f
TheFirstLairron <zrwall@hotmail.com>
2015-11-17 15:43:03 -0800

    Initial Commit

---
+++ presentationForm/presentationForm/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace presentationForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonClicker = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTotalClickingGod = new System.Windows.Forms.Label();
+            this.lblTotalAlienGamer = new System.Windows.Forms.Label();
+            this.lblTotalClickFactory = new System.Windows.Forms.Label();
+            this.lblTotalWOWPlayer = new System.Windows.Forms.Label();
+            this.lblTotalClickFarm = new System.Windows.Forms.Label();
+            this.lblTotalButtonCursor = new System.Windows.Forms.Label();
+            this.btnClickingGod = new System.Windows.Forms.Button();
+            this.btnAlienGamer = new System.Windows.Forms.Button();
+            this.btnClickFactory = new System.Windows.Forms.Button();
+            this.btnWOWPlayer = new System.Windows.Forms.Button();
+            this.btnClickFarm = new System.Windows.Forms.Button();
+            this.btnCursor = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txbxTotalClicks = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClicker
+            // 
+            this.buttonClicker.Location = new System.Drawing.Point(12, 12);
+            this.buttonClicker.Name = "buttonClicker";
+            this.buttonClicker.Size = new System.Drawing.Size(290, 404);
+            this.buttonClicker.TabIndex = 0;
+            this.buttonClicker.Text = "Click Me";
+            this.buttonClicker.UseVisualStyleBackColor = true;
+            this.buttonClicker.Click += new System.EventHandler(this.buttonClicker_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label13);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.lblTotalClickingGod);
+            this.groupBox1.Controls.Add(this.lblTotalAlienGamer);
+            this.groupBox1.Controls.Add(this.lblTotalClickFactory);
+            this.groupBox1.Controls.Add(this.lblTotalWOWPlayer);
+            this.groupBox1.Controls.Add(this.lblTotalClickFarm);
+            this.groupBox1.Controls.Add(this.lblTotalButtonCursor);
+            this.groupBox1.Controls.Add(this.btnClickingGod);
+            this.groupBox1.Controls.Add(this.btnAlienGamer);
+            this.groupBox1.Controls.Add(this.btnClickFactory);
+            this.groupBox1.Controls.Add(this.btnWOWPlayer);
+            this.groupBox1.Controls.Add(this.btnClickFarm);
+            this.groupBox1.Controls.Add(this.btnCursor);
+            this.groupBox1.Location = new System.Drawing.Point(308, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 527);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Upgrades";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(182, 472);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(124, 17);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "Cost: 1000000000";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(182, 387);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(108, 17);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Cost: 10000000";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(182, 304);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(92, 17);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Cost: 100000";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(182, 216);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 17);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "Cost: 1000";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(182, 131);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(68, 17);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Cost: 100";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(182, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(60, 17);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Cost: 10";
+            // 
+            // lblTotalClickingGod
+            // 
+            this.lblTotalClickingGod.AutoSize = true;
+            this.lblTotalClickingGod.Location = new System.Drawing.Point(329, 472);
+            this.lblTotalClickingGod.Name = "lblTotalClickingGod";
+            this.lblTotalClickingGod.Size = new System.Drawing.Size(16, 17);
+            this.lblTotalClickingGod.TabIndex = 11;
+            this.lblTotalClickingGod.Text = "0";
+            // 
+            // lblTotalAlienGamer
+            // 
+            this.lblTotalAlienGamer.AutoSize = true;
+            this.lblTotalAlienGamer.Location = new System.Drawing.Point(329, 387);
+            this.lblTotalAlienGamer.Name = "lblTotalAlienGamer";
+            this.lblTotalAlienGamer.Size = new System.Drawing.Size(16, 17);
+            this.lblTotalAlienGamer.TabIndex = 10;
+            this.lblTotalAlienGamer.Text = "0";
+            // 
+            // lblTotalClickFactory
+            // 
+            this.lblTotalClickFactory.AutoSize = true;
+            this.lblTotalClickFactory.Location = new System.Drawing.Point(329, 304);
+            this.lblTotalClickFactory.Name = "lblTotalClickFactory";
+            this.lblTotalClickFactory.Size = new System.Drawing.Size(16, 17);
+            this.lblTotalClickFactory.TabIndex = 9;
+            this.lblTotalClickFactory.Text = "0";
+            // 
+            // lblTotalWOWPlayer
+            // 
+            this.lblTotalWOWPlayer.AutoSize = true;
+            this.lblTotalWOWPlayer.Location = new System.Drawing.Point(329, 216);
+            this.lblTotalWOWPlayer.Name = "lblTotalWOWPlayer";
+            this.lblTotalWOWPlayer.Size = new System.Drawing.Size(16, 17);
+            this.lblTotalWOWPlayer.TabIndex = 8;
+            this.lblTotalWOWPlayer.Text = "0";
+            // 
+            // lblTotalClickFarm
+            // 
+            this.lblTotalClickFarm.AutoSize = true;
+            this.lblTotalClickFarm.Location = new System.Drawing.Point(329, 131);
+            this.lblTotalClickFarm.Name = "lblTotalClickFarm";
+            this.lblTotalClickFarm.Size = new System.Drawing.Size(16, 17);
+            this.lblTotalClickFarm.TabIndex = 7;
+            this.lblTotalClickFarm.Text = "0";
+            // 
+            // lblTotalButtonCursor
+            // 
+            this.lblTotalButtonCursor.AutoSize = true;
+            this.lblTotalButtonCursor.Location = new System.Drawing.Point(329, 46);
+            this.lblTotalButtonCursor.Name = "lblTotalButtonCursor";
+            this.lblTotalButtonCursor.Size = new System.Drawing.Size(16, 17);
+            this.lblTotalButtonCursor.TabIndex = 6;
+            this.lblTotalButtonCursor.Text = "0";
+            // 
+            // btnClickingGod
+            // 
+            this.btnClickingGod.Enabled = false;
+            this.btnClickingGod.Location = new System.Drawing.Point(6, 447);
+            this.btnClickingGod.Name = "btnClickingGod";
+            this.btnClickingGod.Size = new System.Drawing.Size(170, 67);
+            this.btnClickingGod.TabIndex = 5;
+            this.btnClickingGod.Text = "Clicking God";
+            this.btnClickingGod.UseVisualStyleBackColor = true;
+            this.btnClickingGod.Click += new System.EventHandler(this.btnClickingGod_Click);
+            // 
+            // btnAlienGamer
+            // 
+            this.btnAlienGamer.Enabled = false;
+            this.btnAlienGamer.Location = new System.Drawing.Point(6, 362);
+            this.btnAlienGamer.Name = "btnAlienGamer";
+            this.btnAlienGamer.Size = new System.Drawing.Size(170, 67);
+            this.btnAlienGamer.TabIndex = 4;
+            this.btnAlienGamer.Text = "Alien Gamer";
+            this.btnAlienGamer.UseVisualStyleBackColor = true;
+            this.btnAlienGamer.Click += new System.EventHandler(this.btnAlienGamer_Click);
+            // 
+            // btnClickFactory
+            // 
+            this.btnClickFactory.Enabled = false;
+            this.btnClickFactory.Location = new System.Drawing.Point(6, 279);
+            this.btnClickFactory.Name = "btnClickFactory";
+            this.btnClickFactory.Size = new System.Drawing.Size(170, 67);
+            this.btnClickFactory.TabIndex = 3;
+            this.btnClickFactory.Text = "Click Factory";
+            this.btnClickFactory.UseVisualStyleBackColor = true;
+            this.btnClickFactory.Click += new System.EventHandler(this.btnClickFactory_Click);
+            // 
+            // btnWOWPlayer
+            // 
+            this.btnWOWPlayer.Enabled = false;
+            this.btnWOWPlayer.Location = new System.Drawing.Point(6, 191);
+            this.btnWOWPlayer.Name = "btnWOWPlayer";
+            this.btnWOWPlayer.Size = new System.Drawing.Size(170, 67);
+            this.btnWOWPlayer.TabIndex = 2;
+            this.btnWOWPlayer.Text = "WOW Player";
+            this.btnWOWPlayer.UseVisualStyleBackColor = true;
+            this.btnWOWPlayer.Click += new System.EventHandler(this.btnWOWPlayer_Click);
+            // 
+            // btnClickFarm
+            // 
+            this.btnClickFarm.Enabled = false;
+            this.btnClickFarm.Location = new System.Drawing.Point(6, 106);
+            this.btnClickFarm.Name = "btnClickFarm";
+            this.btnClickFarm.Size = new System.Drawing.Size(170, 67);
+            this.btnClickFarm.TabIndex = 1;
+            this.btnClickFarm.Text = "Click Farm";
+            this.btnClickFarm.UseVisualStyleBackColor = true;
+            this.btnClickFarm.Click += new System.EventHandler(this.btnClickFarm_Click);
+            // 
+            // btnCursor
+            // 
+            this.btnCursor.Enabled = false;
+            this.btnCursor.Location = new System.Drawing.Point(6, 21);
+            this.btnCursor.Name = "btnCursor";
+            this.btnCursor.Size = new System.Drawing.Size(170, 67);
+            this.btnCursor.TabIndex = 0;
+            this.btnCursor.Text = "Button Cursor";
+            this.btnCursor.UseVisualStyleBackColor = true;
+            this.btnCursor.Click += new System.EventHandler(this.btnCursor_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 434);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Clicks:";
+            // 
+            // txbxTotalClicks
+            // 
+            this.txbxTotalClicks.Location = new System.Drawing.Point(66, 431);
+            this.txbxTotalClicks.Name = "txbxTotalClicks";
+            this.txbxTotalClicks.ReadOnly = true;
+            this.txbxTotalClicks.Size = new System.Drawing.Size(236, 22);
+            this.txbxTotalClicks.TabIndex = 3;
+            this.txbxTotalClicks.Text = "0";
+            this.txbxTotalClicks.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 542);
+            this.Controls.Add(this.txbxTotalClicks);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonClicker);
+            this.Name = "Form1";
+            this.Text = "Button Clicker";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClicker;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClickingGod;
+        private System.Windows.Forms.Button btnAlienGamer;
+        private System.Windows.Forms.Button btnClickFactory;
+        private System.Windows.Forms.Button btnWOWPlayer;
+        private System.Windows.Forms.Button btnClickFarm;
+        private System.Windows.Forms.Button btnCursor;
+        private System.Windows.Forms.Label lblTotalButtonCursor;
+        private System.Windows.Forms.Label lblTotalClickingGod;
+        private System.Windows.Forms.Label lblTotalAlienGamer;
+        private System.Windows.Forms.Label lblTotalClickFactory;
+        private System.Windows.Forms.Label lblTotalWOWPlayer;
+        private System.Windows.Forms.Label lblTotalClickFarm;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txbxTotalClicks;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ presentationForm/presentationForm/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace presentationForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClicker_Click(object sender, EventArgs e)
+        {
+            UInt64 total = getTotal();
+            total++;
+            setTotal(total);
+            enableButtons();
+        }
+
+        public UInt64 getTotal()
+        {
+            string strTotal = (this.txbxTotalClicks.Text);
+            UInt64 total = Convert.ToUInt64(strTotal);
+            return total;
+        }
+
+        public void setTotal(UInt64 newTotal)
+        {
+            string strTotal = (this.txbxTotalClicks.Text);
+            UInt64 total = Convert.ToUInt64(strTotal);
+            total = newTotal;
+            this.txbxTotalClicks.Text = total.ToString();
+        }
+
+        public void enableButtons()
+        {
+            UInt64 total = getTotal();
+            if (total >= 10)
+            {
+                btnCursor.Enabled = true;
+            }
+            else
+            {
+                btnCursor.Enabled = false;
+            }
+
+            if(total >= 100)
+            {
+                btnClickFarm.Enabled = true;
+            }
+            else
+            {
+                btnClickFarm.Enabled = false;
+            }
+
+            if(total >= 1000)
+            {
+                btnWOWPlayer.Enabled = true;
+            }
+            else
+            {
+                btnWOWPlayer.Enabled = false;
+            }
+
+            if (total >= 10000)
+            {
+                btnClickFactory.Enabled = true;
+            }
+            else
+            {
+                btnClickFactory.Enabled = false;
+            }
+
+            if (total >= 100000)
+            {
+                btnAlienGamer.Enabled = true;
+            }
+            else
+            {
+                btnAlienGamer.Enabled = false;
+            }
+
+            if(total >= 1000000)
+            {
+                btnClickingGod.Enabled = true;
+            }
+            else
+            {
+                btnClickingGod.Enabled = false;
+            }
+        }
+
+        public bool buyUpgrade(UInt64 cost, Label label)
+        {
+            UInt64 total = getTotal();
+            if(total >= cost)
+            {
+                setTotal(total - cost);
+                string lblText = label.Text;
+                int lblInt = Convert.ToInt32(lblText);
+                lblInt++;
+                label.Text = lblInt.ToString();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UInt64 total = getTotal();
+            UInt64 totalFromUpgrades = 0;
+
+            string LBCText = lblTotalButtonCursor.Text;
+            UInt64 LBCInt = Convert.ToUInt64(LBCText);
+            totalFromUpgrades += LBCInt;
+
+            string LCFText = lblTotalClickFarm.Text;
+            UInt64 LCFInt = Convert.ToUInt64(LCFText);
+            totalFromUpgrades += LCFInt * 2;
+
+            string LWPText = lblTotalWOWPlayer.Text;
+            UInt64 LWPInt = Convert.ToUInt64(LWPText);
+            totalFromUpgrades += LWPInt * 5;
+
+            string LCFAText = lblTotalClickFactory.Text;
+            UInt64 LCFAInt = Convert.ToUInt64(LCFAText);
+            totalFromUpgrades += LWPInt * 10;
+
+            string LAGText = lblTotalAlienGamer.Text;
+            UInt64 LAGInt = Convert.ToUInt64(LAGText);
+            totalFromUpgrades += LAGInt * 100;
+
+            string LCGText = lblTotalClickingGod.Text;
+            UInt64 LCGInt = Convert.ToUInt64(LCGText);
+            totalFromUpgrades += LCGInt * 1000;
+
+            setTotal(totalFromUpgrades + total);
+            enableButtons();
+        }
+
+        private void btnCursor_Click(object sender, EventArgs e)
+        {
+            string lblText = lblTotalButtonCursor.Text;
+            int lblInt = Convert.ToInt32(lblText);
+            if (lblInt >= 10)
+            {
+                MessageBox.Show("You can\'t buy any more of those.");
+            }
+            else
+            {
+                buyUpgrade(10, lblTotalButtonCursor);
+                enableButtons();
+            }            
+        }
+
+        private void btnClickFarm_Click(object sender, EventArgs e)
+        {
+            string lblText = lblTotalClickFarm.Text;
+            int lblInt = Convert.ToInt32(lblText);
+            if (lblInt >= 10)
+            {
+                MessageBox.Show("You can\'t buy any more of those.");
+            }
+            else
+            {
+                buyUpgrade(100, lblTotalClickFarm);
+                enableButtons();
+            }
+        }
+
+        private void btnWOWPlayer_Click(object sender, EventArgs e)
+        {
+            string lblText = lblTotalWOWPlayer.Text;
+            int lblInt = Convert.ToInt32(lblText);
+            if (lblInt >= 10)
+            {
+                MessageBox.Show("You can\'t buy any more of those.");
+            }
+            else
+            {
+                buyUpgrade(1000, lblTotalWOWPlayer);
+                enableButtons();
+            }
+        }
+
+        private void btnClickFactory_Click(object sender, EventArgs e)
+        {
+            string lblText = lblTotalClickFactory.Text;
+            int lblInt = Convert.ToInt32(lblText);
+            if (lblInt >= 10)
+            {
+                MessageBox.Show("You can\'t buy any more of those.");
+            }
+            else
+            {
+                buyUpgrade(100000, lblTotalClickFactory);
+                enableButtons();
+            }
+        }
+
+        private void btnAlienGamer_Click(object sender, EventArgs e)
+        {
+            string lblText = lblTotalAlienGamer.Text;
+            int lblInt = Convert.ToInt32(lblText);
+            if (lblInt >= 10)
+            {
+                MessageBox.Show("You can\'t buy any more of those.");
+            }
+            else
+            {
+                buyUpgrade(10000000, lblTotalAlienGamer);
+                enableButtons();
+            }
+        }
+
+        private void btnClickingGod_Click(object sender, EventArgs e)
+        {
+            string lblText = lblTotalClickingGod.Text;
+            int lblInt = Convert.ToInt32(lblText);
+            if (lblInt >= 10)
+            {
+                MessageBox.Show("You can\'t buy any more of those.");
+            }
+            else
+            {
+                buyUpgrade(1000000000, lblTotalClickingGod);
+                enableButtons();
+            }
+        }
+    }
+}
